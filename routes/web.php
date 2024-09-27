ee666f9d4e8e6ffcbeff549a72750b0720b8821c
Lucas Correia <lucasdevproject@gmail.com>
2024-09-27 13:45:56 -0300

    Introdução a Rotas no Laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::any('/any', function(){
+    return "Permite todo tipo de acesso http(get,post,delete,put)";
+});
+
+Route::match(['get','post'], '/match', function(){
+    return "Permite apenas acessos definidos";
+});
+
+Route::get('/produto/{id}/{cat}', function($id, $cat){
+    return "O id do produto é:".$id."<br>"."A categoria é:".$cat;
+});
+
+Route::redirect('/sobre', '/empresa');
+
+Route::view('/empresa','/empresa');
+
+Route::get('/holynews', function(){
+    return view('news');
+})->name('noticias');
+
+Route::get('/thenovidades', function(){
+    return redirect()->route('noticias');
+});
+
+
+
+Route::prefix('admin')->group(function(){
+    Route::get('dashboard', function () {
+        return 'dashboard';
+    });
+
+    Route::get('users', function () {
+        return 'users';
+    });
+
+    Route::get('clientes', function () {
+        return 'clientes';
+    });
+});
